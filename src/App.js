4eb3a20a745ac1c527b480ec3b291fa42c7bdb3c
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-02 15:30:00 +0300

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutUs from './pages/AboutUs'
+import GLobalStyle from './components/GlobalStyle'
 
 function App() {
   return (
     <div className="App">
+    <GLobalStyle />
      <AboutUs />
     </div>
   );
