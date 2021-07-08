67efe5a4418aaf5d7178e3ae16dc5f7f9051ce11
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-08 15:29:32 +0300

    added font

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 
 const Nav = () => {
     return (
-        <nav>
+        <StyledNav>
             <h1><a href="#">Capture</a></h1>
             <ul>
                 <li><a>1. About Us</a></li>
                 <li><a>2. Our Work</a></li>
                 <li><a>3. Contact Us</a></li>
             </ul>
-        </nav>
+        </StyledNav>
     )
 }
 
@@ -19,6 +19,22 @@ min-height:10vh;
 display:flex;
 margin:auto;
 justify-content: space-between;
+align-items: center;
+padding:1rem 10rem;
+background: #282828;
+a {
+    color: white;
+    text-decoration:none;
+}
+ul {
+    display:flex;
+    list-style:none;
+}
+h1 {
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+}
 `
 
 export default Nav
