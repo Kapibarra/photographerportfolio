32b9527e5d35ec3cca12f561b8406c7d17b8b685
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-19 13:47:31 +0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
-import AboutUs from './pages/AboutUs'
-import GLobalStyle from './components/GlobalStyle'
-import Nav from './components/Nav'
+import AboutUs from "./pages/AboutUs";
+import GLobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+// router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <GLobalStyle />
-    <Nav />
-     <AboutUs />
+      <GLobalStyle />
+      <Nav />
+      <Route path="/about">
+        <AboutUs />
+      </Route>
+      <Route path="/work">
+        <OurWork />
+      </Route>
+      <Route path="/contact">
+        <ContactUs />
+      </Route>
+
+      <OurWork />
     </div>
   );
 }
