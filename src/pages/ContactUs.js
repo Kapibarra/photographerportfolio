84ed8e648dd1d416b4f9afcc3b3b19ed4ae84aac
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-20 14:33:50 +0300

    added contact us animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// framer motion
+import {motion} from "framer-motion"
+import {pageAnimation} from '../Animation'
 
 const ContactUs = () => {
     return (
-        <div>
+        <motion.div variants={pageAnimation} initial="hidden" animate="show" exit="exit">
             <h1>ContactUs</h1>
-        </div>
+        </motion.div>
     )
 }
 
