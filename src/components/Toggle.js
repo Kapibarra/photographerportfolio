8ff13d5f8b395cb116affcd33dabed3a1c1358b1
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-26 13:37:46 +0300

    fixes

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -4,7 +4,7 @@ import {useState} from 'react'
 
 
 const Toggle = ({children , title}) => {
-    const [toggle, setToggle] = useState(true)
+    const [toggle, setToggle] = useState(false)
     return (
 <motion.div layout className="question" onClick={() => setToggle(!toggle)}>
 <motion.h4 layout>{title}</motion.h4>
